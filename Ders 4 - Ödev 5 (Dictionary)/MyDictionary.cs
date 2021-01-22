695a763fa9f692df399d9f30663da2e7484bce3a
Serhan Altug <saltug@yahoo.com>
2021-01-22 13:40:52 +0300

    Ders 4 - Ödev 5

---
+++ Ders 4 - Ödev 5 (Dictionary)/MyDictionary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ders_4_Odev_5
+{
+    class MyDictionary<T1, T2>
+    {
+        T1[] keys;
+        T2[] values;
+
+        public MyDictionary()
+        {
+            keys = new T1[0];
+            values = new T2[0];
+        }
+
+        public void Add(T1 Key, T2 Value) 
+        {
+            T1[] tempArrayKeys = keys;
+            T2[] tempArrayValues = values;
+
+            keys = new T1[keys.Length + 1];
+            values = new T2[keys.Length + 1];
+
+            for (int i = 0; i < tempArrayKeys.Length; i++)
+            {
+                keys[i] = tempArrayKeys[i];
+                values[i] = tempArrayValues[i];
+            }
+
+            keys[keys.Length - 1] = Key;
+            values[keys.Length - 1] = Value;
+        }
+    }
+}
---
+++ Ders 4 - Ödev 5 (Dictionary)/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ders_4_Odev_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = MyDictionary<int, string>();
+
+            myDictionary.Add(1, "Serhan");
+            myDictionary.Add(2, "Zeynep");
+            myDictionary.Add(5, "Ahmet");
+        }
+    }
+}
